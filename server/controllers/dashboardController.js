03cb8dfe9da3adbee0676e433e04a01637bceca4
locdoan <doanphucloc23@gmail.com>
2025-02-11 11:12:10 +0700

    fix add timetable

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,11 +1,12 @@
 const query = require("../db/queries");
 const {Timetable} = require('../db/model');
+const mongoose = require('mongoose');
 const addNewTimeTable = async (req, res) => {
     try {
       const { user, timeline, content, type } = req.body;
       console.log("body:",req.body)
       const newTimeTable = new Timetable({
-        user,
+        user: new mongoose.Types.ObjectId(user),
         timeline,
         content,
         type,
